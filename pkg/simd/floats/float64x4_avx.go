0b6666be250fbf1a49a2846526d4e5bfe767429f
DaanV2 <daanverstraten@hotmail.com>
2026-08-29 18:28:26 +0200

    fix: something

--- pkg/simd/floats/float64x4_avx.go
+++ pkg/simd/floats/float64x4_avx.go
@@ -11,112 +11,92 @@ type Float64x4 struct {
 func NewFloat64x4(data []float64) Float64x4 {
 	var result Float64x4
 
-	var n int
-	result.data, n = archsimd.LoadFloat64x4Part(data)
-	if n == 4 { // Read atleast 4 points, so there should be more
-		result.data1, _ = archsimd.LoadFloat64x4Part(data[4:])
-	}
+	result.data, _ = archsimd.LoadFloat64x4Part(data)
 
 	return result
 }
 
 func (x Float64x4) Store(receiver []float64) {
-	switch {
-	case len(receiver) == float64_x4_len:
-		x.data.Store(receiver[:(float64_x4_len / 2)])
-		x.data1.Store(receiver[(float64_x4_len / 2):])
-	case len(receiver) > (float64_x4_len / 2):
-		x.data.Store(receiver[:(float64_x4_len / 2)])
-		_ = x.data1.StorePart(receiver[(float64_x4_len / 2):])
-	default:
-		_ = x.data.StorePart(receiver)
+	if len(receiver) >= float64_x4_len {
+		x.data.Store(receiver[:float64_x4_len])
+	} else {
+		x.data.StorePart(receiver)
 	}
 }
 
 // Abs returns the absolute values of the elements of x
 func (x Float64x4) Abs() Float64x4 {
 	return Float64x4{
-		data:  x.data.Abs(),
-		data1: x.data1.Abs(),
+		data: x.data.Abs(),
 	}
 }
 
 // Add performs a fused: x + y.
 func (x Float64x4) Add(y Float64x4) Float64x4 {
 	return Float64x4{
-		data:  x.data.Add(y.data),
-		data1: x.data1.Add(y.data1),
+		data: x.data.Add(y.data),
 	}
 }
 
 // Div performs a fused: x / y.
 func (x Float64x4) Div(y Float64x4) Float64x4 {
 	return Float64x4{
-		data:  x.data.Div(y.data),
-		data1: x.data1.Div(y.data1),
+		data: x.data.Div(y.data),
 	}
 }
 
 // Mul performs a fused: x * y.
 func (x Float64x4) Mul(y Float64x4) Float64x4 {
 	return Float64x4{
-		data:  x.data.Mul(y.data),
-		data1: x.data1.Mul(y.data1),
+		data: x.data.Mul(y.data),
 	}
 }
 
 // MulAdd performs a fused: (x * y) + z.
 func (x Float64x4) MulAdd(y, z Float64x4) Float64x4 {
 	return Float64x4{
-		data:  x.data.MulAdd(y.data, z.data),
-		data1: x.data1.MulAdd(y.data1, z.data1),
+		data: x.data.MulAdd(y.data, z.data),
 	}
 }
 
 // Max computes the maximum of each pair of corresponding elements in x and y.
 func (x Float64x4) Max(y Float64x4) Float64x4 {
 	return Float64x4{
-		data:  x.data.Max(y.data),
-		data1: x.data1.Max(y.data1),
+		data: x.data.Max(y.data),
 	}
 }
 
 // Min computes the minimum of each pair of corresponding elements in x and y.
 func (x Float64x4) Min(y Float64x4) Float64x4 {
 	return Float64x4{
-		data:  x.data.Min(y.data),
-		data1: x.data1.Min(y.data1),
+		data: x.data.Min(y.data),
 	}
 }
 
 // Neg returns the negation of the elements of x
 func (x Float64x4) Neg() Float64x4 {
 	return Float64x4{
-		data:  x.data.Neg(),
-		data1: x.data1.Neg(),
+		data: x.data.Neg(),
 	}
 }
 
 // Scale multiplies each element of x by 2 raised to the power of the floor of the corresponding element in y.
 func (x Float64x4) Scale(y Float64x4) Float64x4 {
 	return Float64x4{
-		data:  x.data.Scale(y.data),
-		data1: x.data1.Scale(y.data1),
+		data: x.data.Scale(y.data),
 	}
 }
 
 // Sub performs a fused: x - y.
 func (x Float64x4) Sub(y Float64x4) Float64x4 {
 	return Float64x4{
-		data:  x.data.Sub(y.data),
-		data1: x.data1.Sub(y.data1),
+		data: x.data.Sub(y.data),
 	}
 }
 
 // Sqrt computes the square root of each element.
 func (x Float64x4) Sqrt() Float64x4 {
 	return Float64x4{
-		data:  x.data.Sqrt(),
-		data1: x.data1.Sqrt(),
+		data: x.data.Sqrt(),
 	}
 }
