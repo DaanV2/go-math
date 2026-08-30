812f9d485f6fe68304300c02fcc501a399ab1cbf
DaanV2 <daanverstraten@hotmail.com>
2026-08-30 09:46:43 +0200

    feat: adding float64x2

---
+++ pkg/simd/floats/float64x2_avx256.go
@@ -0,0 +1,21 @@
+//go:build simd_avx256
+
+package simdfloats
+
+import "math"
+
+// Scale multiplies each element of x by 2 raised to the power of the floor of the corresponding element in y.
+func (x Float64x2) Scale(y Float64x2) Float64x2 {
+	var result [float64_x2_len]float64
+	var vx [float64_x2_len]float64
+	var vy [float64_x2_len]float64
+
+	x.Store(vx[:])
+	y.Store(vy[:])
+
+	for i := range vx {
+		result[i] = vx[i] * math.Pow(2, vy[i])
+	}
+
+	return NewFloat64x2(result[:])
+}
